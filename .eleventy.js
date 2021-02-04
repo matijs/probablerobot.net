69ae873a832b1e6b5350a3d8ed6d142a1b83536f
Matijs Brinkhuis <matijs@gmail.com>
2021-02-04 12:53:19 +0100

    Watch 'src/utils' as well
    Pick up changes in 'src/utils'.
    
    [skip netlify]

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,9 @@ const { humanReadableDate, isoDate } = require('./src/utils/date.js');
 
 module.exports = (config) => {
   config.addPlugin(syntaxHighlight);
-  config.addPassthroughCopy(`src/site/css`);
-  config.addPassthroughCopy(`src/site/robots.txt`);
+  config.addPassthroughCopy('src/site/css');
+  config.addPassthroughCopy('src/site/robots.txt');
+  config.addWatchTarget('src/utils');
 
   config.addNunjucksFilter('humanReadableDate', humanReadableDate);
   config.addNunjucksFilter('isoDate', isoDate);
