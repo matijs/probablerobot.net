741c3f825086c5e6b0283648c68ec98084d0e759
Matijs Brinkhuis <matijs@gmail.com>
2021-02-14 20:16:31 +0100

    Simplify CSS
    Use rem units instead of em units.
    Make color variables usable without having to call hsl()
    Remove 1 mediaquery, simplify the other
    Update package-lock.json

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const { humanReadableDate, isoDate } = require('./src/utils/date.js');
 
 module.exports = (config) => {
   config.addPlugin(syntaxHighlight);
-  config.addPassthroughCopy('src/site/css/styles.css');
+  config.addPassthroughCopy('src/site/css/');
   config.addPassthroughCopy('src/site/robots.txt');
   config.addWatchTarget('src/utils');
 
