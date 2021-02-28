70cc6a5a2bef51c414d18efc513edcea29f63b56
Matijs Brinkhuis <matijs@gmail.com>
2021-02-28 13:35:23 +0100

    Make footnotes great again

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,16 @@
+const markdownIt = require('markdown-it');
+const markdownItFootnote = require('markdown-it-footnote');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const { humanReadableDate, isoDate } = require('./src/utils/date.js');
 
 module.exports = (config) => {
+  config.setLibrary(
+    'md',
+    markdownIt({
+      html: true,
+      linkify: false,
+    }).use(markdownItFootnote)
+  );
   config.addPlugin(syntaxHighlight);
   config.addPassthroughCopy('src/site/css/');
   config.addPassthroughCopy('src/site/robots.txt');
