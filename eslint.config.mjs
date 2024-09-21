7ff302f352556169a3f5b7e49603874827bea2be
matijs <matijs@gmail.com>
2024-09-21 20:13:14 +0200

    build: update eslint config
    - add linterOptions to report unused disable directives
    - rename pluginJs -> js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,14 @@
+import js from '@eslint/js';
 import globals from 'globals';
-import pluginJs from '@eslint/js';
 
 export default [
   {
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
     },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
   },
-  pluginJs.configs.recommended,
+  js.configs.recommended,
 ];
