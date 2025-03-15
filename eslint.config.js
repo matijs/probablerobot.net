5513d95cbbc89acf415b76d16b6e4fc6335f5efa
matijs <matijs@gmail.com>
2025-03-15 13:51:41 +0100

    refactor: eslint config
    Use the new `defineConfig` and `globalIgnores` from `eslint/config`

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,29 @@
 import js from '@eslint/js';
 import json from '@eslint/json';
 import perfectionist from 'eslint-plugin-perfectionist';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 
-export default [
-  {
-    ignores: ['**/public/'],
-  },
+export default defineConfig([
+  globalIgnores(['**/public/']),
   {
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+      globals: { ...globals.node },
     },
   },
   {
+    extends: ['js/recommended'],
     files: ['**/*.js'],
-    ...js.configs.recommended,
-    ...perfectionist.configs['recommended-natural'],
+    plugins: { js },
   },
   {
+    ...perfectionist.configs['recommended-natural'],
+    files: ['**/*.js'],
+  },
+  {
+    extends: ['json/recommended'],
     files: ['**/*.json'],
     language: 'json/json',
-    ...json.configs.recommended,
+    plugins: { json },
   },
-];
+]);
